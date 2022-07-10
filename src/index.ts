7371b9e04fb3126d80f6bef54edae19213f7a801
atomcorp <thomasmaxwellsmith@gmail.com>
2022-07-10 20:37:57 +0100

    add file to s3 bucket

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,12 @@
 import { APIGatewayProxyEvent } from "aws-lambda";
+
 import handleResponse from "./handleResponse";
-
-import requests from "./requests";
-
-const helloworld = "Hello World! I am listening";
-
-const myFunc = (str: string) => str;
-
-console.log(myFunc(helloworld));
-
-const test = async () => {
-  try {
-    const responses = await requests();
-    const data = handleResponse(responses);
-    console.log(data);
-  } catch (error) {
-    console.error("Error start !!!!!!!!!!!");
-    console.error(error);
-    console.error("Error end !!!!!!!!!!");
-  }
-};
+import handleRequests from "./handleRequests";
+import putObjectToS3 from "./utils/uploadToS3";
 
 exports.handler = async (event: APIGatewayProxyEvent) => {
-  const content = await test();
-  // TODO implement
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify(content),
-  };
+  const responses = await handleRequests();
+  const data = handleResponse(responses);
 
-  return response;
+  putObjectToS3(data);
 };
---
+++ src/utils/uploadToS3.ts
@@ -0,0 +1,23 @@
+// Load the SDK for JavaScript
+import AWS from "aws-sdk";
+import { WindowsTerminalThemeWithMetaType } from "../types";
+
+AWS.config.update({ region: "eu-west-2" });
+
+const putObjectToS3 = (data: WindowsTerminalThemeWithMetaType[]) => {
+  const s3 = new AWS.S3();
+  const params = {
+    Bucket: "themesjson",
+    Key: "themes.json",
+    Body: JSON.stringify(data),
+  };
+  s3.putObject(params, function (err, data) {
+    if (err) {
+      console.log(err, err.stack);
+    } else {
+      console.log(data);
+    }
+  });
+};
+
+export default putObjectToS3;
